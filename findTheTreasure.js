b6eae3451dcfa0a8fc92191a4d61519002e9f445
noeladrian5 <85713320+noeladrian5@users.noreply.github.com>
2022-07-06 17:14:35 -0500

    Add files via upload
    update as of 7/6

--- findTheTreasure.js
+++ findTheTreasure.js
@@ -1,46 +1,88 @@
-function moveForward(x, y, direction) {
-    return { 
-        position: [x, y],
-        direction: onTurnRight[direction] ?? direction
+function moveForward(startingPosition, direction) {  
+    console.log("moving forward"); 
+    switch (direction) {
+        case 'North': {
+            startingPosition = [startingPosition[0], startingPosition[1] + 1]
+            return {startingPosition};
+        }
+        case 'South': {
+            startingPosition = [startingPosition[0], startingPosition[1] - 1]
+            return {startingPosition};
+        }
+        case 'East': {
+            startingPosition = [startingPosition[0] + 1, startingPosition[1] ]
+            return {startingPosition};
+        }
+        case 'West': {
+            startingPosition = [startingPosition[0] - 1, startingPosition[1] ]
+            return {startingPosition};
+        }
     }
 }
 
-const onMoveForward = {
-    //todo
-}
-
-const onTurnRight = {
-    'North': 'East',
-    'South': 'West',
-    'East' : 'South',
-    'West' : 'North',
-}
-
-const onTurnLeft = {
-    'North': 'West',
-    'South': 'East',
-    'West' : 'South',
-    'East' : 'North',
-}
-
-function turnRight(x, y, direction) {
-    return { 
-        position: [x, y],
-        direction: onTurnRight[direction] ?? direction
+function turnRight( direction) {
+    switch (direction) {
+        case 'North': {
+            const newDirection = 'East';
+            return {newDirection};
+        }
+        case 'South': {
+            const newDirection = 'West';
+            return {newDirection};
+        }
+        case 'East': {
+            const newDirection = 'South';
+            return {newDirection};
+        }
+        case 'West': {
+            const newDirection = 'North';
+            return {newDirection};
+        }
     }
+
 }
 
-function turnLeft(x, y, direction) {
-    return { 
-        position: [x, y],
-        direction: onTurnRight[direction] ?? direction
+function turnLeft( direction) {
+    switch (direction) {
+        case 'North': {
+            const newDirection = 'West';
+            return {newDirection};
+        }
+        case 'South': {
+            const newDirection = 'East';
+            return {newDirection};
+        }
+        case 'East': {
+            const newDirection = 'North';
+            return {newDirection};
+        }
+        case 'West': {
+            const newDirection = 'South';
+            return {newDirection};
+        }
     }
 }
 
 function findTheTreasure(startingPosition, direction, instructions) {
-    //TODO implement
+    instructions.forEach(element => {
+        switch (element) {
+            case 'Forward': {
+                startingPosition = moveForward(startingPosition, direction);      
+                return {startingPosition};        
+            }
+            case 'Right': {
+                direction = turnRight(direction);
+                return {direction};
+            }
+            case 'Left': {
+                direction = turnLeft(direction);
+                return {direction};
+            }
+        }
+    });
+    console.log("I am starting at", startingPosition, " and facing", direction)
+    turnRight(startingPosition, direction, instructions)
 }
-
 // CTRL+D on Windows is multi select
 
 
